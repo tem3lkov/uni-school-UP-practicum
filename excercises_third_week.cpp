700a8736b52dd7b30642121e042ff82cbd2d0790
temelk0vtodor <115074162+temelk0vtodor@users.noreply.github.com>
2022-10-26 13:02:56 +0300

    Update excercises_third_week.cpp

--- excercises_third_week.cpp
+++ excercises_third_week.cpp
@@ -23,5 +23,20 @@ int main()
         std::cout << (i*i + 3*i) << "; ";
     }
     return 0
+        
+    // Third Task -------------------------
+    int n = 0;
+    int temp = INT_MIN; // returns the lowest possible int
+    int number = 0;
+
+    std::cin >> n;
+
+    for(int i = 0; i < n; i++) 
+    {
+        std::cin >> number;
+        temp = (number < 0 && number > temp)  ? number : temp;
+    }
+
+    std::cout << temp << std::endl;
 }
 
