755abe93055ef2198519d764c56082e6800764dc
temelk0vtodor <115074162+temelk0vtodor@users.noreply.github.com>
2022-11-02 13:04:01 +0200

    Update excercises_forth_week.cpp

--- excercises_forth_week.cpp
+++ excercises_forth_week.cpp
@@ -20,12 +20,49 @@ int main()
     int tempNumber = 0;
     cin >> num;
 
-    // 177
-    // 6771
 
     while(num > 0) {   
         reversedNumber= reversedNumber*10 + (num%10);
         num/=10;
     }
     cout << reversedNumber;
+  
+  // Task 12
+
+    unsigned int num = 0;
+    cin >> num;
+
+    int size = 0;
+    unsigned int finalNum = 0;
+    unsigned int copyNum = 0;
+    while(copyNum > 0) {
+        size++;
+        copyNum /=10;
+    }
+    int middle = size / 2;
+    
+    unsigned int firstPart = 0;
+    unsigned int secondPart = 0;
+
+    int step = 1;
+
+    for(int i = 0; i < middle; i++) {
+        secondPart = secondPart + (num%10) * step;
+        num/=10;
+        step *= 10;
+    }
+
+    firstPart = num;
+    
+    int powerOfTen = 1;
+    int firstHalfSize = (size + 1) / 2;
+    for(int i = 1; i <= firstHalfSize; i++) {
+        powerOfTen = powerOfTen * 10;
+    }
+
+    finalNum = secondPart * powerOfTen + firstPart;
+
+    cout << finalNum;
+
+
 }
